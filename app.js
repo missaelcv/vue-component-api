9c2dd7b7d0e10b068f820f7f90e92dcdf36757cb
missaelcv <missaelcv0220@gmail.com>
2022-03-17 22:55:19 -0400

    methods modal

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-Vue.component('modal',{
+Vue.component('modal', {
     props: ['title'],
-
-    methods:{
-        close(){
-            this.$emit('close')
-        }
+    
+    methods: {
+      close () {
+        this.$emit('close')
+      }
     },
-
+    
     template: `
-    <div class="modal-mask">
-    <div class=modal-wrapper>
-        <div class = "moda-container">
-        <h3>{{ title }}</h3>
-        <div>
-        <slot name="body"></slot>
-        </div>
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <h3>{{ title }}</h3>
+            <div>
+              <slot name="body"></slot>
+            </div>
             <footer>
-                <button v-on:click="close">Cerrar<button>
-            <flooter>
-                </div>
-                </div>
-                </div>
-    `
-})
-
-new Vue({
-    el:'#app',
-            data() {
-                return {
-                    title: 'El Titulo del model',
-                    showModal: false
-                }
-            },
-            
-})
+              <button v-on:click="close">Cerrar</button>
+            </footer>
+          </div>
+        </div>
+      </div>`
+  })
+  
+  new Vue({
+    el: '#app',
+    
+    data () {
+      return {
+        title: 'Titulo del modal',
+        showModal: false
+      }
+    },
+    
+    methods: {
+      toggleModal () {
+        this.showModal = !this.showModal
+      }
+    }
+  })
